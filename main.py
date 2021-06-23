b5f3768ce6adaee6bcb5ce0aeaa2e26bae12aaa2
MuskiHacker <83163286+MuskiHacker@users.noreply.github.com>
2021-06-23 17:33:51 +0000

    Create main.py

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ def p():
     print "\033[1;97mSECOND STEP OF LOGIN"
     print "\033[1;97m--------------------"
     print "\033[1;97m "
-    print("          \033[1;94mTOOL USERNAME : Muski Tools (correct)")
+    print("          \033[1;92mTOOL USERNAME : Muski Tools (correct)")
     pwd=raw_input("          \033[1;93mTOOL PASSWORD : ")
     if pwd == cpwd:
         z()
@@ -86,8 +86,8 @@ def p():
         print "\033[1;97mSECOND STEP OF LOGIN"
         print "\033[1;97m--------------------"
         print "\033[1;97m "
-        print("          \033[1;94mTOOL USERNAME : Muski Tools (correct)")
-        print("          \033[1;93mTOOL PASSWORD : "+pwd+" (wrong)")
+        print("          \033[1;92mTOOL USERNAME : Muski Tools (correct)")
+        print("          \033[1;92mTOOL PASSWORD : "+pwd+" (wrong)")
         time.sleep(1)
         os.system('xdg-open https://www.facebook.com/groups/231747098048450')
         p()
@@ -98,8 +98,8 @@ def z():
     print "\033[1;97mSECOND STEP OF LOGIN"
     print "\033[1;97m--------------------"
     print "\033[1;97m "
-    print("          \033[1;94mTOOL USERNAME : Muski Tools (correct)")
-    print("          \033[1;93mTOOL PASSWORD : Muski Tools (correct)")
+    print("          \033[1;92mTOOL USERNAME : Muski Tools (correct)")
+    print("          \033[1;92mTOOL PASSWORD : Muski Tools (correct)")
     print(" \033[1;92mLogin Successfull\033[0m")
     jalan("\033[1;93mPlease Wait 5 Minutes, All Packages Are Checking.....")
   
